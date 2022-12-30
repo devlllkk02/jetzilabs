324abb23bd5bfd7357ca65fbfa2e9603f1a5d480
Naveen Liyanage <devlllkk02@gmail.com>
2022-12-30 09:35:35 +0530

    Notifications: Added delete icon

--- src/components/Notifications/Notifications.tsx
+++ src/components/Notifications/Notifications.tsx
@@ -35,102 +35,12 @@ function Notifications() {
               <div className="notifications__mint__content__body__middle">
                 <p>15 mins</p>
               </div>
-              <div className="notifications__mint__content__body__right"></div>
-            </div>
-          </div>
-          {/* Notification 2 */}
-          <div className="notifications__mint">
-            <div className="notifications__mint__content__body">
-              <div className="notifications__mint__content__body__left">
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
-                  est ab atque omnis molestiae ullam dolore quae modi aperiam,
-                  asperiores corrupti doloribus. Nemo a officiis odio, nobis
-                  obcaecati aperiam id debitis iste non, qui similique dicta
-                  itaque quis minus deserunt dolor aut, adipisci nesciunt unde
-                  amet. Earum deserunt assumenda voluptas?
-                </p>
+              <div className="notifications__mint__content__body__right">
+                <img
+                  src="./images/sidebar_icons/delete-svgrepo-com.svg"
+                  alt=""
+                />
               </div>
-              <div className="notifications__mint__content__body__middle">
-                <p>15 mins</p>
-              </div>
-              <div className="notifications__mint__content__body__right"></div>
-            </div>
-          </div>{" "}
-          {/* Notification 3 */}
-          <div className="notifications__mint">
-            <div className="notifications__mint__content__body">
-              <div className="notifications__mint__content__body__left">
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
-                  est ab atque omnis molestiae ullam dolore quae modi aperiam,
-                  asperiores corrupti doloribus. Nemo a officiis odio, nobis
-                  obcaecati aperiam id debitis iste non, qui similique dicta
-                  itaque quis minus deserunt dolor aut, adipisci nesciunt unde
-                  amet. Earum deserunt assumenda voluptas?
-                </p>
-              </div>
-              <div className="notifications__mint__content__body__middle">
-                <p>15 mins</p>
-              </div>
-              <div className="notifications__mint__content__body__right"></div>
-            </div>
-          </div>{" "}
-          {/* Notification 4 */}
-          <div className="notifications__mint">
-            <div className="notifications__mint__content__body">
-              <div className="notifications__mint__content__body__left">
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
-                  est ab atque omnis molestiae ullam dolore quae modi aperiam,
-                  asperiores corrupti doloribus. Nemo a officiis odio, nobis
-                  obcaecati aperiam id debitis iste non, qui similique dicta
-                  itaque quis minus deserunt dolor aut, adipisci nesciunt unde
-                  amet. Earum deserunt assumenda voluptas?
-                </p>
-              </div>
-              <div className="notifications__mint__content__body__middle">
-                <p>15 mins</p>
-              </div>
-              <div className="notifications__mint__content__body__right"></div>
-            </div>
-          </div>{" "}
-          {/* Notification 5 */}
-          <div className="notifications__mint">
-            <div className="notifications__mint__content__body">
-              <div className="notifications__mint__content__body__left">
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
-                  est ab atque omnis molestiae ullam dolore quae modi aperiam,
-                  asperiores corrupti doloribus. Nemo a officiis odio, nobis
-                  obcaecati aperiam id debitis iste non, qui similique dicta
-                  itaque quis minus deserunt dolor aut, adipisci nesciunt unde
-                  amet. Earum deserunt assumenda voluptas?
-                </p>
-              </div>
-              <div className="notifications__mint__content__body__middle">
-                <p>15 mins</p>
-              </div>
-              <div className="notifications__mint__content__body__right"></div>
-            </div>
-          </div>{" "}
-          {/* Notification 6 */}
-          <div className="notifications__mint">
-            <div className="notifications__mint__content__body">
-              <div className="notifications__mint__content__body__left">
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
-                  est ab atque omnis molestiae ullam dolore quae modi aperiam,
-                  asperiores corrupti doloribus. Nemo a officiis odio, nobis
-                  obcaecati aperiam id debitis iste non, qui similique dicta
-                  itaque quis minus deserunt dolor aut, adipisci nesciunt unde
-                  amet. Earum deserunt assumenda voluptas?
-                </p>
-              </div>
-              <div className="notifications__mint__content__body__middle">
-                <p>15 mins</p>
-              </div>
-              <div className="notifications__mint__content__body__right"></div>
             </div>
           </div>
         </div>
