47b39ee4caac0ee71eddaf2fa3bfb4f038fd46af
Naveen Liyanage <devlllkk02@gmail.com>
2022-12-30 09:32:00 +0530

    Tasks: Added icons

--- src/components/Tasks/Tasks.tsx
+++ src/components/Tasks/Tasks.tsx
@@ -75,69 +75,18 @@ function Tasks() {
               </div>
             </div>
             <div className="tasks__mint__controls">
-              <div className="tasks__mint__controls__top"></div>
-              <div className="tasks__mint__controls__bottom"></div>
-            </div>
-          </div>
-
-          {/* Mint 1 */}
-          <div className="tasks__mint">
-            <div className="tasks__mint__content">
-              <div className="tasks__mint__content__header">
-                <div className="tasks__mint__content__header__topic">
-                  <p>Mint</p>
-                </div>
-                <div className="tasks__mint__content__header__icon">
-                  <div className="tasks__mint__content__header__icon__container">
-                    <p>Selected Wallet</p>
-                  </div>
-                </div>
+              <div className="tasks__mint__controls__top">
+                <img
+                  src="./images/sidebar_icons/pause-circle-solid-svgrepo-com.svg"
+                  alt=""
+                />
               </div>
-              <div className="tasks__mint__content__body">
-                <div className="tasks__mint__content__body__r1">
-                  <div className="tasks__mint__content__body__r1__item1">
-                    <p>Contract Address</p>
-                    <p>0x686c626E48bfC5DC98a30a9992897766fed4Abd3</p>
-                  </div>
-                  <div className="tasks__mint__content__body__r1__item2">
-                    <p>NFT Price</p>
-                    <p>5000 ETH</p>
-                  </div>
-                  <div className="tasks__mint__content__body__r1__item3">
-                    <p>Mint Amount</p>
-                    <p>05/10</p>
-                  </div>
-                </div>
-                <div className="tasks__mint__content__body__r2">
-                  <div className="tasks__mint__content__body__r2__item1">
-                    <p>Created Time</p>
-                    <p>5d ago</p>
-                  </div>
-                  <div className="tasks__mint__content__body__r2__item2">
-                    <p>NFT Transfer Address</p>
-                    <p>0x686c626E48bfC5DC98a30a9992897766fed4Abd3</p>
-                  </div>
-                  <div className="tasks__mint__content__body__r2__item3">
-                    <p>Gas Setting</p>
-                    <p>Medium</p>
-                  </div>
-                </div>
+              <div className="tasks__mint__controls__bottom">
+                <img
+                  src="./images/sidebar_icons/delete-svgrepo-com.svg"
+                  alt=""
+                />
               </div>
-              <div className="tasks__mint__content__footer">
-                <div className="tasks__mint__content__footer__icon">
-                  <div className="tasks__mint__content__footer__icon__container">
-                    <div></div>
-                    <p>Active</p>
-                  </div>
-                </div>
-                <div className="tasks__mint__content__footer__text">
-                  <p>Amount 0/5</p>
-                </div>
-              </div>
-            </div>
-            <div className="tasks__mint__controls">
-              <div className="tasks__mint__controls__top"></div>
-              <div className="tasks__mint__controls__bottom"></div>
             </div>
           </div>
           {/* Mint 1 */}
@@ -196,69 +145,18 @@ function Tasks() {
               </div>
             </div>
             <div className="tasks__mint__controls">
-              <div className="tasks__mint__controls__top"></div>
-              <div className="tasks__mint__controls__bottom"></div>
-            </div>
-          </div>
-
-          {/* Mint 1 */}
-          <div className="tasks__mint">
-            <div className="tasks__mint__content">
-              <div className="tasks__mint__content__header">
-                <div className="tasks__mint__content__header__topic">
-                  <p>Mint</p>
-                </div>
-                <div className="tasks__mint__content__header__icon">
-                  <div className="tasks__mint__content__header__icon__container">
-                    <p>Selected Wallet</p>
-                  </div>
-                </div>
+              <div className="tasks__mint__controls__top">
+                <img
+                  src="./images/sidebar_icons/pause-circle-solid-svgrepo-com.svg"
+                  alt=""
+                />
               </div>
-              <div className="tasks__mint__content__body">
-                <div className="tasks__mint__content__body__r1">
-                  <div className="tasks__mint__content__body__r1__item1">
-                    <p>Contract Address</p>
-                    <p>0x686c626E48bfC5DC98a30a9992897766fed4Abd3</p>
-                  </div>
-                  <div className="tasks__mint__content__body__r1__item2">
-                    <p>NFT Price</p>
-                    <p>5000 ETH</p>
-                  </div>
-                  <div className="tasks__mint__content__body__r1__item3">
-                    <p>Mint Amount</p>
-                    <p>05/10</p>
-                  </div>
-                </div>
-                <div className="tasks__mint__content__body__r2">
-                  <div className="tasks__mint__content__body__r2__item1">
-                    <p>Created Time</p>
-                    <p>5d ago</p>
-                  </div>
-                  <div className="tasks__mint__content__body__r2__item2">
-                    <p>NFT Transfer Address</p>
-                    <p>0x686c626E48bfC5DC98a30a9992897766fed4Abd3</p>
-                  </div>
-                  <div className="tasks__mint__content__body__r2__item3">
-                    <p>Gas Setting</p>
-                    <p>Medium</p>
-                  </div>
-                </div>
+              <div className="tasks__mint__controls__bottom">
+                <img
+                  src="./images/sidebar_icons/delete-svgrepo-com.svg"
+                  alt=""
+                />
               </div>
-              <div className="tasks__mint__content__footer">
-                <div className="tasks__mint__content__footer__icon">
-                  <div className="tasks__mint__content__footer__icon__container">
-                    <div></div>
-                    <p>Active</p>
-                  </div>
-                </div>
-                <div className="tasks__mint__content__footer__text">
-                  <p>Amount 0/5</p>
-                </div>
-              </div>
-            </div>
-            <div className="tasks__mint__controls">
-              <div className="tasks__mint__controls__top"></div>
-              <div className="tasks__mint__controls__bottom"></div>
             </div>
           </div>
           {/* Mint 1 */}
@@ -317,11 +215,20 @@ function Tasks() {
               </div>
             </div>
             <div className="tasks__mint__controls">
-              <div className="tasks__mint__controls__top"></div>
-              <div className="tasks__mint__controls__bottom"></div>
+              <div className="tasks__mint__controls__top">
+                <img
+                  src="./images/sidebar_icons/pause-circle-solid-svgrepo-com.svg"
+                  alt=""
+                />
+              </div>
+              <div className="tasks__mint__controls__bottom">
+                <img
+                  src="./images/sidebar_icons/delete-svgrepo-com.svg"
+                  alt=""
+                />
+              </div>
             </div>
           </div>
-
           {/* Mint 1 */}
           <div className="tasks__mint">
             <div className="tasks__mint__content">
@@ -378,8 +285,88 @@ function Tasks() {
               </div>
             </div>
             <div className="tasks__mint__controls">
-              <div className="tasks__mint__controls__top"></div>
-              <div className="tasks__mint__controls__bottom"></div>
+              <div className="tasks__mint__controls__top">
+                <img
+                  src="./images/sidebar_icons/pause-circle-solid-svgrepo-com.svg"
+                  alt=""
+                />
+              </div>
+              <div className="tasks__mint__controls__bottom">
+                <img
+                  src="./images/sidebar_icons/delete-svgrepo-com.svg"
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+          {/* Mint 1 */}
+          <div className="tasks__mint">
+            <div className="tasks__mint__content">
+              <div className="tasks__mint__content__header">
+                <div className="tasks__mint__content__header__topic">
+                  <p>Mint</p>
+                </div>
+                <div className="tasks__mint__content__header__icon">
+                  <div className="tasks__mint__content__header__icon__container">
+                    <p>Selected Wallet</p>
+                  </div>
+                </div>
+              </div>
+              <div className="tasks__mint__content__body">
+                <div className="tasks__mint__content__body__r1">
+                  <div className="tasks__mint__content__body__r1__item1">
+                    <p>Contract Address</p>
+                    <p>0x686c626E48bfC5DC98a30a9992897766fed4Abd3</p>
+                  </div>
+                  <div className="tasks__mint__content__body__r1__item2">
+                    <p>NFT Price</p>
+                    <p>5000 ETH</p>
+                  </div>
+                  <div className="tasks__mint__content__body__r1__item3">
+                    <p>Mint Amount</p>
+                    <p>05/10</p>
+                  </div>
+                </div>
+                <div className="tasks__mint__content__body__r2">
+                  <div className="tasks__mint__content__body__r2__item1">
+                    <p>Created Time</p>
+                    <p>5d ago</p>
+                  </div>
+                  <div className="tasks__mint__content__body__r2__item2">
+                    <p>NFT Transfer Address</p>
+                    <p>0x686c626E48bfC5DC98a30a9992897766fed4Abd3</p>
+                  </div>
+                  <div className="tasks__mint__content__body__r2__item3">
+                    <p>Gas Setting</p>
+                    <p>Medium</p>
+                  </div>
+                </div>
+              </div>
+              <div className="tasks__mint__content__footer">
+                <div className="tasks__mint__content__footer__icon">
+                  <div className="tasks__mint__content__footer__icon__container">
+                    <div></div>
+                    <p>Active</p>
+                  </div>
+                </div>
+                <div className="tasks__mint__content__footer__text">
+                  <p>Amount 0/5</p>
+                </div>
+              </div>
+            </div>
+            <div className="tasks__mint__controls">
+              <div className="tasks__mint__controls__top">
+                <img
+                  src="./images/sidebar_icons/pause-circle-solid-svgrepo-com.svg"
+                  alt=""
+                />
+              </div>
+              <div className="tasks__mint__controls__bottom">
+                <img
+                  src="./images/sidebar_icons/delete-svgrepo-com.svg"
+                  alt=""
+                />
+              </div>
             </div>
           </div>
         </div>
